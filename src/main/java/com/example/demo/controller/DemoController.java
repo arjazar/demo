cb98e81373c0b5bde7da46d36b7c58d4a1f0bc3c
arjazar <abdourahman.jazar@gmail.com>
2023-07-04 14:49:37 +0300

    initial commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome";
+    }
+
+
+}
